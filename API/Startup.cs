2fcdec862643fd59517dd25d60ded5ea569b84f1
Donte Jean-Pierre <dontejp@gmail.com>
2022-08-11 15:51:56 -0400

    added CORS support

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,13 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"); //support for cors ... also needs middleware
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +47,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");//middleware for cors
 
             app.UseAuthorization();
 
